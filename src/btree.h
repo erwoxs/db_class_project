2a426ef1564d24c828aadd3436669e1dee486dd0
rayanelammari <rayane.lammari.sio@gmail.com>
2024-11-06 17:51:21 +0100

    Add update and delete instruction in btree.h and repl.h

--- src/btree.h
+++ src/btree.h
@@ -1,25 +1,26 @@
-// btree.h
 #ifndef BTREE_H
 #define BTREE_H
 #include <stdbool.h>
 #include "repl.h"
 
-// Définir une structure de noeud pour chaque noeud de l'arbre binaire
+// Définir une structure de noeud dans l'arbre binaire
 typedef struct Node {
-    Row data;              // Row data
-    struct Node* left;     // Pointeur vers le noeud gauche
-    struct Node* right;    // Pointeur vers le noeud droit
+    Row data;              // Données du noeud en ligne
+    struct Node* left;     // Pointeur vers le noeud enfant gauche
+    struct Node* right;    // Pointeur vers le noeud enfant droit
 } Node;
 
-// Définir une structure de table contenant la racine de l'arbre binaire
+// Définir une structure de table de la base de données
 typedef struct Table {
-    Node* root;            // Racine de l'arbre binaire pour accéder à tous les autres nœuds de l'arbre
+    Node* root;            //pointeur vers la racine de l'arbre binaire
 } Table;
 
 // Prototypes de fonctions pour manipuler l'arbre binaire
 Table* new_table();                  // Crée une nouvelle instance de la table
 void table_insert(Table* table, Row data); // Insère une ligne dans l'arbre binaire
-Row* table_select(Table* table, int id);   // Recherche une ligne par id
+Row* table_select(Table* table, int id);   // Recherche une ligne par id dans l'arbre binaire
+void table_update(Table* table, int id, const char* name, const char* email); //Modifie la ligne en utilisant l'id pour modifier le nom ou l'email
+void table_delete(Table* table, int id); //Supprime une ligne en recherchant l'id
 void free_table(Table* table);             // Libère la mémoire allouée pour chaque noeud
 
 #endif
--- src/repl.h
+++ src/repl.h
@@ -6,7 +6,7 @@
 
 
 
-// Définir une structure row (ligne) qui contient les données à insérer
+// Définir une structure row qui contient les données à insérer ou afficher
 typedef struct {
     int id;                // id
     char name[255];        // name
@@ -15,49 +15,51 @@ typedef struct {
 
 
 
-//Énumérations pour les commandes
+//Enumération pour les commandes comme .exit saisit par l'utilisateur
 typedef enum {
     META_COMMAND_SUCCESS,
     META_COMMAND_UNRECOGNIZED_COMMAND
-} MetaCommandResult; //commandes en mode repl 
+} MetaCommandResult;
 
 
-//Déclaration de l'énumération pour les résultats de commande préparée
+//Enumération pour les résultats de commande préparée
 typedef enum { 
     PREPARE_SUCCESS, 
     PREPARE_UNRECOGNIZED_STATEMENT 
-} PrepareResult;    //pour vérifier le succès ou l'échec de la préparation de la commande.
+} PrepareResult;   
 
 
-//Déclaration des types de statements
+//Enumération des types de statements
 typedef enum { 
     STATEMENT_INSERT, 
-    STATEMENT_SELECT 
-} StatementType;   //pour définir le type de commande
+    STATEMENT_SELECT,
+    STATEMENT_UPDATE,
+    STATEMENT_DELETE
+} StatementType;   
 
 //Déclaration de la structure Statement pour représenter une instruction
 typedef struct{
     StatementType type;
-    Row row; //Inclure la ligne dans l'instruction
+    Row row; 
 }Statement;
 
 
 //Déclaration de la structure InputBuffer pour gérer l'entrée utilisateur
 typedef struct {
-    char* buffer;
-    size_t buffer_length;
-    size_t input_length;
-} InputBuffer;  //représente le buffer qui contient l'entrée utilisateur.
+    char* buffer; //stocke l'entée utilisateur
+    size_t buffer_length; //taille de l'entrée en mémoire
+    size_t input_length; //longueur de l'entrée 
+} InputBuffer;
 
 
 //Prototypes de fonctions
-InputBuffer* new_input_buffer();
-void print_prompt();
-void read_input(InputBuffer* input_buffer);
-void close_input_buffer(InputBuffer* input_buffer);
-MetaCommandResult do_meta_command(InputBuffer* input_buffer);
-PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement);
-void execute_statement(Statement* statement);
-void repl(void);
+InputBuffer* new_input_buffer(); //Crée et initalise un tampon d'entrée
+void print_prompt(); //Affiche l'invite de commande 
+void read_input(InputBuffer* input_buffer); //Lit l'entrée utilisateur et la stocke dans input_buffer
+void close_input_buffer(InputBuffer* input_buffer); //Libère la mémoire allouée à input_buffer avce free()
+MetaCommandResult do_meta_command(InputBuffer* input_buffer); //Lit input_buffer pour identifier si l'utilisateur a entré une commande méta.
+PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement); //Analyse l'entrée utilisateur pour voir si elle correspond à une instruction 
+void execute_statement(Statement* statement); //Execute l'instruction en fonction de son type.
+void repl(void); //démarre la boucle repl
 
 #endif // REPL_H
