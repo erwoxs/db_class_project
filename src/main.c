fbd5913f0a8fbbb946d4b72882e9cf2eaa4865a0
Lammari <123374793+rayanelammari@users.noreply.github.com>
2024-11-18 13:30:51 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -2,16 +2,16 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
-#include "repl.h" // Inclure le fichier d'en-tête repl.h
+#include "repl.h" 
 #include "dpersistence.h"
 #include "btree.h"
 
 int main(int argc, char* argv[], char* envp[]){
   
-  // Charge la table depuis le fichier "table_data.dat" si elle existe
+  //Charge la table depuis le fichier "table_data.dat" s'il existe
   Table* table = load_table_from_file("table_data.dat");
   if (table == NULL) {
-    // Si le fichier n'existe pas, crée une nouvelle table
+    //Si le fichier n'existe pas, crée une nouvelle table
     table = new_table();
   }else 
   {
@@ -24,7 +24,7 @@ int main(int argc, char* argv[], char* envp[]){
   save_table_to_file(table, "table_data.dat");
   printf("Table sauvegardée dans le fichier.\n");
 
-  // Libère la mémoire de la table
+  //Libère la mémoire de la table
   free(table);
   return 0;
   
