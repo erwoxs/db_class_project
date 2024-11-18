c4e969cb5774d4b647320b47d4c87def5d08fb9c
Lammari <123374793+rayanelammari@users.noreply.github.com>
2024-11-18 13:28:03 +0100

    Update btree.h

--- src/btree.h
+++ src/btree.h
@@ -3,14 +3,14 @@
 #include <stdbool.h>
 #include "repl.h"
 
-// Définir une structure de noeud dans l'arbre binaire
+//Définir une structure de noeud dans l'arbre binaire
 typedef struct Node {
-    Row data;              // Données du noeud en ligne
-    struct Node* left;     // Pointeur vers le noeud enfant gauche
-    struct Node* right;    // Pointeur vers le noeud enfant droit
+    Row data;              //Données du noeud en ligne
+    struct Node* left;     //Pointeur vers le noeud enfant gauche
+    struct Node* right;    //Pointeur vers le noeud enfant droit
 } Node;
 
-// Définir une structure de table de la base de données
+//Définir une structure de table de la base de données
 typedef struct Table {
     int num_rows;
     Row* rows;
@@ -18,12 +18,12 @@ typedef struct Table {
     
 } Table;
 
-// Prototypes de fonctions pour manipuler l'arbre binaire
-Table* new_table();                  // Crée une nouvelle instance de la table
-void table_insert(Table* table, Row data); // Insère une ligne dans l'arbre binaire
-Row* table_select(Table* table, int id);   // Recherche une ligne par id dans l'arbre binaire
-Row* table_update(Table* table, int id, const char* name); //Modifie la ligne en utilisant l'id pour modifier le nom ou l'email
-void free_table(Table* table);             // Libère la mémoire allouée pour chaque noeud
+//Prototypes de fonctions pour manipuler l'arbre binaire
+Table* new_table();                  //Crée une nouvelle instance de la table
+void table_insert(Table* table, Row data); //Insère une ligne dans l'arbre binaire
+Row* table_select(Table* table, int id);   //Recherche une ligne par id dans l'arbre binaire
+Row* table_update(Table* table, int id, const char* name); //Modifie la ligne en utilisant l'id pour modifier le nom
+void free_table(Table* table);             //Libère la mémoire allouée pour chaque noeud
 
 #endif
 
