8f35a7f2771d3c97346c9c62983df4393d193a44
Lammari <123374793+rayanelammari@users.noreply.github.com>
2024-11-18 13:30:16 +0100

    Update dpersistence.h

--- src/dpersistence.h
+++ src/dpersistence.h
@@ -1,9 +1,9 @@
 #ifndef DPERSISTENCE_H
 #define DPERSISTENCE_H
-#include "btree.h"  // Inclure btree.h pour pouvoir utiliser la structure Table
+#include "btree.h"
 
 
-// Déclaration des fonctions de persistance
+//Déclaration des fonctions de persistance
 void save_table_to_file(Table* table, const char* fileData);
 Table* load_table_from_file(const char* fileData);
 
